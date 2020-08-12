b3df052bfee2af8bae532abea563f4ec2068191f
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-08-12 11:33:36 +0800

    Fixed profile styling

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -3,12 +3,24 @@ import Axios from "axios";
 import moment from "moment";
 //material-ui
 import { Box, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: "1rem",
+    background: "#fff",
+    padding: "1rem",
+    borderRadius: "1rem",
+    border: "2px solid black",
+    textAlign: "center",
+  },
+}));
 
 const Profile = (props) => {
   const { match } = props;
   const { params } = match;
   const { id } = params;
-
+  const classes = useStyles();
   const [user, setUser] = useState("");
 
   useEffect(() => {
@@ -31,21 +43,17 @@ const Profile = (props) => {
     //eslint-disable-next-line
   }, []);
 
+
   return (
     <>
       {user ? (
-        <Box component="div">
-          <Typography variant="h6">Username: {user.username}</Typography>
-          <Typography variant="h6">Email: {user.email}</Typography>
-          <Typography variant="h6">
-            Date Created: {moment(user.createdAt).fromNow()}
-          </Typography>
-          <Box component="ul">
-            {user.friends.length === null ? (
-              user.friends.map((friend) => <li>{friend}</li>)
-            ) : (
-              <Typography variant="h6">You have no friends</Typography>
-            )}
+        <Box component="div" className={classes.root}>
+          <Box component="div" className={classes.userInfo}>
+            <Typography variant="h6">Username: {user.username}</Typography>
+            <Typography variant="h6">Email: {user.email}</Typography>
+            <Typography variant="subtitle2">
+              Date Created: {moment(user.createdAt).fromNow()}
+            </Typography>
           </Box>
         </Box>
       ) : (
